86921d178be8ee041a1adb5575793e0d4792eccc
Perrin <jeanpaul.perrin@cgi.com>
2020-02-12 10:48:03 -0500

    add  a  commit for rm resource-gist/springboot-concourse-0.0.5-SNAPSHOT.jar

--- ci/test-and-build.sh
+++ ci/test-and-build.sh
@@ -3,4 +3,10 @@
 set -e -u -
 cd source-code-from-github/
 ./mvnw clean package
-cp ./target/*.jar  ../jar-file
+rm resource-gist/springboot-concourse-0.0.5-SNAPSHOT.jar
+cp code/target/springboot-concourse-0.0.5-SNAPSHOT.jar resource-gist/springboot-concourse-0.0.5-SNAPSHOT.jar
+cd resource-gist
+git config --global user.email "jeanpaul.perrin@cgi.com"
+git config --global user.name "JPP"
+git add .
+git commit -m "update jar springboot-concourse-0.0.5-SNAPSHOT.jar"
