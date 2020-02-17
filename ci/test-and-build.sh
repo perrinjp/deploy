c34bbc4e6a77f4d3fe6b9075f42a46c48de34333
JPP <jeanpaul.perrin@cgi.com>
2020-02-17 16:19:01 -0500

    added new file

--- ci/test-and-build.sh
+++ ci/test-and-build.sh
@@ -5,9 +5,9 @@ cd source-code-from-github/
 ./mvnw clean package
 
 cd ..
-cp -rf source-code-from-github/target/springboot-concourse-0.0.5-SNAPSHOT.jar resource-gist/springboot-concourse-0.0.5-SNAPSHOT.jar
+cp -rf source-code-from-github/target/springboot-concourse-tutorial-0.0.5-SNAPSHOT.jar resource-gist/springboot-concourse-tutorial-0.0.5-SNAPSHOT.jar
 cd resource-gist
 git config --global user.email "jeanpaul.perrin@cgi.com"
 git config --global user.name "JPP"
 git add .
-git commit -m "update jar springboot-concourse-0.0.5-SNAPSHOT.jar"
+git commit -m "update jar springboot-concourse-tutorial-0.0.5-SNAPSHOT.jar"
