9de627180da45bca690a7fbea09641f63d645b5a
JPP <jeanpaul.perrin@cgi.com>
2020-02-18 13:08:58 -0500

    added utf-8

--- src/test/java/com/perrinjp/springboot/concourse/controller/ItemControllerTest.java
+++ src/test/java/com/perrinjp/springboot/concourse/controller/ItemControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import com.perrinjp.tutorial.internalization.MvcConfig;
 
 import java.util.Arrays;
 import java.util.List;
